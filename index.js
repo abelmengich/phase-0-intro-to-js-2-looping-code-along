57969ffc22ad1a9f0a95dc7347405045bbf9fb0a
abel mengich <kipyegoabel2308@gmail.com>
2024-03-22 20:05:04 -0400

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  
+function countDown(startingNumber) {
+    if (startingNumber <= 0) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
+
+
+countDown(10);
